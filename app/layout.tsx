a17b7b25ce36491c25b938789621c2df97314adc
tmokmss <tmokmss@users.noreply.github.com>
2023-08-14 17:26:14 +0900

    fix coi path

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Script src="/coi-serviceworker.min.js" />
+      <Script src="./coi-serviceworker.min.js" />
       <body className={inter.className}>{children}</body>
     </html>
   );
