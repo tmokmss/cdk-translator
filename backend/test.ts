417b3a2d354d45a092329585867e382e9979b479
tmokmss <tmokmss@users.noreply.github.com>
2023-08-15 22:09:26 +0900

    Update test.ts

--- backend/test.ts
+++ backend/test.ts
@@ -1,30 +1,38 @@
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CorsHttpMethod, HttpApi } from '@aws-cdk/aws-apigatewayv2-alpha';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import { DockerImageCode, DockerImageFunction } from 'aws-cdk-lib/aws-lambda';
+import { Platform } from 'aws-cdk-lib/aws-ecr-assets';
 import { Construct } from 'constructs';
-import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import * as cdk from 'aws-cdk-lib';
 
-class TestStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class SampleStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const api = new apigw.RestApi(this, 'cors-api-test', {
-      cloudWatchRole: true,
+    const handler = new DockerImageFunction(this, 'Handler', {
+      code: DockerImageCode.fromImageAsset('backend', { platform: Platform.LINUX_AMD64 }),
+      environment: {
+        NODE_ENV: 'production',
+      },
+      timeout: cdk.Duration.seconds(30),
+      memorySize: 128,
     });
 
-    const handler = new lambda.Function(this, 'handler', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      handler: 'index.handler',
-      code: lambda.Code.fromAsset('integ.cors.handler'),
+    const api = new HttpApi(this, 'Api', {
+      corsPreflight: {
+        allowHeaders: ['*'],
+        allowMethods: [CorsHttpMethod.ANY],
+        allowOrigins: ['*'],
+        maxAge: cdk.Duration.days(1),
+      },
     });
 
-    const twitch = api.root.addResource('twitch');
-    const backend = new apigw.LambdaIntegration(handler);
-    if (true) {
-      twitch.addMethod('GET', backend); // GET /twitch
-    }
-    twitch.addMethod('GET', backend); // GET /twitch
-    twitch.addMethod('POST', backend); // POST /twitch
-    twitch.addMethod('DELETE', backend); // DELETE /twitch
-    twitch.addCorsPreflight({ allowOrigins: ['https://google.com', 'https://www.test-cors.org'] });
+    const integration = new HttpLambdaIntegration('Integration', handler);
+    api.addRoutes({
+      path: '/{proxy+}',
+      integration,
+    });
+
+    new cdk.CfnOutput(this, 'ApiUrl', { value: api.url! });
   }
 }
